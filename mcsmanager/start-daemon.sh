381507f6b288e24172756c76fb188720465bb6af
Lirzh <lirzh1024@163.com>
2025-10-11 17:28:48 +0800

    Delete mcsmanager directory

--- mcsmanager/start-daemon.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd daemon || exit
-node --max-old-space-size=1024 --enable-source-maps app.js
--- mcsmanager/start-web.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd web || exit
-node --max-old-space-size=1024 --enable-source-maps app.js
